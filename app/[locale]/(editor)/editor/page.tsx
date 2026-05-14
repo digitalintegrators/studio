be08652b9366eaf188638b09ff39a11eb6a73f2c
digitalintegrators <info@digitalintegrators.io>
2026-05-14 03:00:55 +0200

    Update page.tsx

--- app/[locale]/(editor)/editor/page.tsx
+++ app/[locale]/(editor)/editor/page.tsx
@@ -2602,7 +2602,7 @@ export default function Editor() {
         setSpotlightFragments((prev) => [...prev, newFragment].sort((a, b) => a.startTime - b.startTime));
         setSelectedSpotlightFragmentId(newFragment.id);
         setSelectedZoomFragmentId(null);
-        setActiveTool("move");
+        setActiveTool("zoom");
     }, [videoDuration]);
 
     const handleUpdateSpotlightFragment = useCallback((fragmentId: string, updates: Partial<SpotlightFragment>) => {
